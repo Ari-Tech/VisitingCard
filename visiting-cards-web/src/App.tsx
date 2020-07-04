d69c58efca00d4e6638e6209cd085dc1041770df
arind <arindamTech@outlook.com>
2020-07-04 23:06:21 +0800

    added image generator from dom of visiting card

--- visiting-cards-web/src/App.tsx
+++ visiting-cards-web/src/App.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
-
+import {VisitingCardComponent} from "./components/VisitingCardComponent";
+import htmlToImage from 'html-to-image';
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+          <p>This is where we will test the Visiting Cards generator</p>
+          <div id={"image-section"}>
+              <VisitingCardComponent/>
+          </div>
 
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+          <button onClick={()=>{
+                const node: any =  document.getElementById('image-section');
+              htmlToImage.toPng(node)
+                  .then(function (dataUrl) {
+                      let img = new Image();
+                      img.src = dataUrl;
+                      document.body.appendChild(img);
+                  })
+                  .catch(function (error) {
+                      console.error('oops, something went wrong!', error);
+                  });
+          }}>get Image</button>
       </header>
     </div>
   );
---
+++ visiting-cards-web/src/components/VisitingCardComponent.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react'
+
+const VisitingCardComponent = (props: any)=>{
+    return <div id={'visiting-card-dom'} className={'visiting-card-dom'}>
+        <div>Arindam Lahiri</div>
+        <div>Front-End Engineer</div>
+        <div>JPMorgan Chase</div>
+        <div/>
+
+    </div>
+}
+
+export {VisitingCardComponent}
